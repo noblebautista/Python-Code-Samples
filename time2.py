15df3f29acd27f81b23a4ecfac76c46a1ddb71e1
Noble B <115103598+noblebautista@users.noreply.github.com>
2022-10-21 14:30:10 -0500

    Update time2.py

--- time2.py
+++ time2.py
@@ -5,10 +5,10 @@
 ########################################################################################
 # Noble Bautista = Editor
 
-str_time = input("What time is it now?")
-str_wait_time = input("What is the number of hours to wait?")
+str_time = input("What time is it now?") #asks for user input (will be variable str_time)
+str_wait_time = input("What is the number of hours to wait?") #asks for user input (will be variable str_wait_time)
 time = int(str_time)
-wait_time = int(str_wait_time)
+wait_time = int(str_wait_time) #defines variables as integers
 
-time_when_start = time + wait_time
-print(time_when_start)
+time_when_start = time + wait_time #formula for time_when_start
+print(time_when_start) #prints start time
