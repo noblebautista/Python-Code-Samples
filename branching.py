ffbcb25d7d4ee591b2ad1874ddd0eab453fe72eb
Noble B <115103598+noblebautista@users.noreply.github.com>
2022-10-21 14:33:30 -0500

    Update branching.py

--- branching.py
+++ branching.py
@@ -8,11 +8,11 @@
 ########################################################################################
 # Noble Bautista = Editor
 
-year = int(input("Greetings! What is your year of origin? "))
+year = int(input("Greetings! What is your year of origin? ")) #asks for user input (specifies it is an integer, defines variable as year).
 
 if year < 1900:
-    print("Woah, that's the past!")
+    print("Woah, that's the past!") #if the user puts in a year before 1900, the message displays.
 elif year >= 1900 & year <= 2020:
-    print("That's totally the present!")
+    print("That's totally the present!") #if the user puts in a year between 1900-2020, the message displays.
 elif year > 2020:
-    print("Far out, that's the future!!")
+    print("Far out, that's the future!!")  #if the user puts in a year after 2020, the message displays.
