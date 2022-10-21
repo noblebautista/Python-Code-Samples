eb203eb17beb4c5a0190f544fe880e44c3fbf381
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-10-21 14:22:48 -0500

    Update Mod3Question6.py

--- Mod3Question6.py
+++ Mod3Question6.py
@@ -9,11 +9,22 @@
 initialBalance = 1000 #1,000 dollars in bank to start with.
 initialBalance = float(initialBalance) #declares that variable must be a number.
 yearlyInterest = 0.05 #5 percent interest annually
-yearlyInterest = float(yearlyInterest) #declares that variable must be a number.
-y0 = initialBalance
+yearlyInterest = float(yearlyInterest) #declares that variable must be a number. #not necessary 
+
+
+#to get balance B for Nth year ends with interest rate R, initial balance P: B = P * ( 1+ N * R)
+y0 = initialBalance   #redundant code
 y1 = initialBalance * yearlyInterest + initialBalance #Algorithm(s) to calculate new net balance
 y2 = y1 * yearlyInterest + initialBalance
 y3 = y2 * yearlyInterest + initialBalance
+
+#ONE, TWO, THREE = 1, 2, 3
+#y1 = initial_bal * ( 1 + rate * ONE)
+#y2 = initial_bal * ( 1 + rate * TWO)
+#y3 = initial_bal * ( 1 + rate * THREE)
+
+
 print("Year one = ", y1) #print statements showing user all yearly balances
+#print("The balance after the first year ends is ${}.".format(y1))
 print("Year two = ", y2)
 print("year three = ", y3)
