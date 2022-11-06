feca47c0f415b426140bc7a89dc93741ce958138
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-06 00:48:25 -0500

    Update Problem4_Mod5Lab.py

--- Problem4_Mod5Lab.py
+++ Problem4_Mod5Lab.py
@@ -9,24 +9,31 @@
 import turtle
 from turtle import Turtle
 
-polygon_sides = [int(input("Please enter the number of sides of the polygon "
-                           "you want the turtle to draw: (as a number) "))]
+#Take a number of sides into single variable instead of a list
+#polygon_sides = [int(input("Please enter the number of sides of the polygon "
+#                           "you want the turtle to draw: (as a number) "))]
+polygon_sides = int(input("Enter the number of sides of a polygon."))
+
 length_side = int(input("Enter the length of the side: (as a number) "))
 color_side = input("Which color do you want the pen to be? ")
 fill_polygon = input("With which color do you want to fill the polygon? ")
 
-    # (above) input statements asking for personalized turtle graphic.
-wn = turtle.Screen()
-wn.bgcolor("darkblue")
+# (above) input statements asking for personalized turtle graphic.
+wn = turtle.Screen() 
+wn.bgcolor("darkblue") 
 noble = turtle.Turtle()
 noble.shape("turtle")
-
+#set colors for a polygon and lines
+noble.color(color_side, fill_polygon)
+ 
 noble.begin_fill()
 
-for i in range(polygon_sides[0]):
-    noble.color(color_side, fill_polygon)
+#for i in range(polygon_sides[0]):
+for i in range(polygon_sides):  #repeat the loop for number of sides
+#    noble.color(color_side, fill_polygon):Use once when to use with begin_fill and end_fill
     noble.forward(length_side)
-    noble.left(180 - (180 * (polygon_sides[0] - 2)) // polygon_sides[0])
+#    noble.left(180 - (180 * (polygon_sides[0] - 2)) // polygon_sides[0])
+    noble.left(360/polygon_sides) #decide turning angles using number of sides
 #  for loop which configures all of the actions (side lengths, colors, etc) onto the turtle.
 noble.end_fill()
 
