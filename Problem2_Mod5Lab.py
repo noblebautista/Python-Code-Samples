a0d87fed174b18aea23222dcc83d383c3926d548
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-06 00:41:17 -0500

    Update Problem2_Mod5Lab.py

--- Problem2_Mod5Lab.py
+++ Problem2_Mod5Lab.py
@@ -9,10 +9,13 @@ nums = [4, 10, 13, 3, 6, 7, 42, 9, 20]  # list of numbers
 for i in nums:
     print(i)  # for loop that prints every number on a new line
 
-cubed = []  # gets number for cubed from list
+#cubed = []  # gets number for cubed from list
+
+#for i in nums:
+#    square = i * i  # formula for squared number
+#    cube = square * i  # formula for cubed number
+#    print("{}       {}      {}".format(i, square, cube))
+    # prints original number from list + squared + cubed on each line
 
 for i in nums:
-    square = i * i  # formula for squared number
-    cube = square * i  # formula for cubed number
-    print("{}       {}      {}".format(i, square, cube))
-    # prints original number from list + squared + cubed on each line
+   print(i, i**2, i**3)
