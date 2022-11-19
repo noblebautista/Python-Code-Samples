39e43b7fcd601a34570333954a8504a64d5a96e7
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-19 16:43:08 -0600

    Update VolOfSphere.py

--- VolOfSphere.py
+++ VolOfSphere.py
@@ -9,7 +9,10 @@ import math  # imports math module
 def VolOfSphere(r): # defines volume of sphere
     return (4 / 3 * math.pi * r ** 3)
     # (above) returns formula for sphere volume
-r = int(input("please enter a to calculate the volume of the sphere: "))
-# (above) defines radius (r) as user input)
-print(VolOfSphere(r))
-# (above) prints sphere volume equation answer using r (user input)
+#try to do modular design
+def main():
+    r = int(input("please enter a to calculate the volume of the sphere: "))
+    # (above) defines radius (r) as user input)
+    print(VolOfSphere(r))
+    # (above) prints sphere volume equation answer using r (user input)
+ main()
