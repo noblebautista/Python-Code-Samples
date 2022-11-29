e9c10308a4399bb8ec313b922b1b6d8710dc4e01
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-11-29 06:41:35 -0600

    Update Mod8Prob2.py

--- Mod8Prob2.py
+++ Mod8Prob2.py
@@ -5,16 +5,23 @@
 # and prints the largest number without using max().
 
 import random  # imports the random module
-num1 = random.randint(1, 30)
-num2 = random.randint(1, 30)
-num3 = random.randint(1, 30)
-# (above) defines number variables that will select a number randomly from given range
-if (num1 >= num2) and (num1 >= num3):
-   largest = num1
-elif (num2 >= num1) and (num2 >= num3):
-   largest = num2
-else:
-   largest = num3
-# (above) if statement (takes three inputs and finds the largest one, returns largest value).
-print(num1, num2, num3)
-print("The largest number is", largest)
+def largestNum(num1, num2, num3):
+   # (above) defines number variables that will select a number randomly from given range
+   if (num1 >= num2) and (num1 >= num3):
+      largest = num1
+   elif (num2 >= num1) and (num2 >= num3):
+      largest = num2
+   else:
+      largest = num3
+   return largest
+
+def main():
+   num1 = random.randint(1, 30)
+   num2 = random.randint(1, 30)
+   num3 = random.randint(1, 30)
+
+   # (above) if statement (takes three inputs and finds the largest one, returns largest value).
+   print(num1, num2, num3)
+   print("The largest number is", largestNum(num1, num2, num3)
+         
+ main()
